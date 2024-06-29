fe333de799abef0f6e98bdc7f602d131cc8d9752
Kshitish <kshitishshetty@gmail.com>
2024-06-29 19:30:24 +0530

    Update GenerateKey.py

--- cryptography/GenerateKey.py
+++ cryptography/GenerateKey.py
@@ -1,6 +1,28 @@
 from cryptography.fernet import Fernet
+import json
 
-key = Fernet.generate_key()
-file = open("encryption_key.txt", 'wb')
-file.write(key)
-file.close()
+
+
+try:
+    with open('config.json', 'r+') as f:  # Open file in read and write mode ('r+')
+
+        config = json.load(f)
+        backup = f.read()
+
+        key = Fernet.generate_key().decode() # Generate key then decode bytes into String
+        config['key'] = key
+
+        f.seek(0)  # Move cursor to the beginning of the file
+        json.dump(config, f, indent=2)  # Write updated config dictionary back to file
+
+        f.truncate()  # If new content is shorter, truncate the remaining part
+    
+    print("Key Generated Successfully!!!")
+    print("Updated .json File")
+except Exception as e:
+    with open('config.json', 'r+') as f:
+        f.write(backup)
+        
+        f.truncate()
+        
+    print("ERROR:", e)
